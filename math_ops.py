dae011e118c03549c7da6f13a2b770c939a8f481
Othman Al Taie <hugoaltaie@gmail.com>
2024-11-17 23:02:54 -0500

    Added the ultimate function to new feature branch!

--- math_ops.py
+++ math_ops.py
@@ -4,8 +4,14 @@ def divide_numbers(a, b):
     """Divides two numbers and returns the result."""
     return a / b  # Potential division by zero error
 
+def ultimate_function():
+    print("I am the ultimate function! Haha! Prepare!")
+    print("No one is superior to me!")
+    print("I am not one... not two... but THREE print statements!")
+
 if __name__ == "__main__":
     x = 10
     y = 0
     result = divide_numbers(x, y)
     print(f"The result of division is: {result}")
+    ultimate_function()
